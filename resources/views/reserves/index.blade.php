052dda1f42f69011166ebbe04ee498244ce9a089
ishikawahironori <issy.yukyou@gmail.com>
2022-02-28 10:39:25 +0900

    first commit

---
+++ resources/views/reserves/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Reservation_system</title>
+</head>
+<body>
+  <header>
+    <nav class="my-navbar">
+      <a class="my-navbar-brand" href="#">予約システム</a>
+      <a href="/logout">ログアウト</a>
+    </nav>
+  </header>
+  <form>
+    <label>予約方法を選択してください。</label>
+    <input type="radio" name="reserves" value="来社予約" name="reserves">来社予約
+    <input type="radio" name="reserves" value="リモート予約" name="remote">リモート予約
+    <a href="#" class="btn btn-default btn-block">送信する</a>
+  </form>
+</body>
+</html>
